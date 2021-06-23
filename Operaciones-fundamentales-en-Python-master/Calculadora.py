96ad09934936850ddc5e44a6a0dfb7c2d0a4a35d
Luis Alberto <luisalbertoislascastillo@gmail.com>
2021-06-23 15:59:21 -0500

    Operaciones

---
+++ Operaciones-fundamentales-en-Python-master/Calculadora.py
@@ -0,0 +1,34 @@
+x=int(raw_input("Ingresa el primer numero"))
+b=int(raw_input("Ingresa el segundo numero"))
+t=x+b
+
+print "La suma es=",t
+
+
+a=int(raw_input("Ingresa el primer numero"))
+c=int(raw_input("Ingresa el segundo numero"))
+g=a-b
+
+print "La resta es=",g
+
+
+
+z=int(raw_input("Ingresa el primer numero"))
+b=int(raw_input("Ingresa el segundo numero"))
+l=z*b
+
+print "La multiplicacion es=",l
+
+
+p=int(raw_input("Ingresa el primer numero"))
+j=int(raw_input("Ingresa el segundo numero"))
+h=p/j
+
+print "La divicion es=",h
+
+
+d=int(raw_input("Ingresa el primer numero"))
+s=int(raw_input("Ingresa el segundo numero"))
+v=d%s
+
+print "El residuo es=",v
---
+++ Operaciones-fundamentales-en-Python-master/README.md
@@ -0,0 +1,3 @@
+# Operaciones-fundamentales-en-Python.
+Creación de una pequeña aplicación que nos permite realizar las operaciones aritméticas fundamentales.
+Programa valido para la versión de python-2.7.13
